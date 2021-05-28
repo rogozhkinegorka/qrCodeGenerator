84fd3e536117b3b60b9a10fad8f1a31c26bd941b
rogozhkinegorka <rogozhkinegorka@gmail.com>
2021-05-28 13:53:32 +0300

    simple design and structure, buttonClick

--- app/src/main/java/com/example/qr_codegenerator/MainActivity.java
+++ app/src/main/java/com/example/qr_codegenerator/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.qr_codegenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView appName;
+    private TextView text;
+    private EditText inputText;
+    private Button generateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appName = findViewById(R.id.appName);
+        text = findViewById(R.id.text);
+        inputText = findViewById(R.id.inputText);
+        generateButton = findViewById(R.id.generateButton);
+
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Kolya Loh", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
